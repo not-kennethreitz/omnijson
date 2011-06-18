d581d126f74fa3cd6da139e7f90534d0bec08920
Kenneth Reitz <me@kennethreitz.com>
2011-06-18 14:08:59 -0400

    all set

--- omnijson/core.py
+++ omnijson/core.py
@@ -1,37 +1,68 @@
 # -*- coding: utf-8 -*-
 
 from .packages import simplejson as internal_json
+from .packages.simplejson.ordered_dict import OrderedDict
 
 engine = None
 _engine = None
 
+# ujson, dumps, loads
 
-# ordered dict ?
+json_map = OrderedDict()
+json_map['simplejson_from_packages'] = ['loads', 'dumps', ValueError]
+
+json_map['ujson'] = ['loads', 'dumps', ValueError]
+json_map['yajl'] = ['loads', 'dumps', (TypeError, ValueError)]
+json_map['jsonlib2'] = ['read', 'write', ValueError]
+json_map['jsonlib'] = ['read', 'write', ValueError]
+json_map['simplejson'] = ['loads', 'dumps', (TypeError, ValueError)]
+json_map['json'] = ['loads', 'dumps', (TypeError, ValueError)]
+
+json_map['simplejson_from_packages'] = ['loads', 'dumps', ValueError]
+# json_map['packages.simplejson'] = ['simplejson.loads', 'simplejson.dumps', ValueError]
+
 
 # from .packages import simplejson
 
-def _import(engine, from_mod=False):
+def _import(engine):
     try:
-        if from_mod:
-            # from from_mod import engine
-            __import__(engine)
-        else:
-            __import__(engine)
+        if '_from_' in engine:
+            engine, package = engine.split('_from_')
+            # print package
+            # print engine
+            # print '\o/'
+            # __import__(engine, fromlist=package)
+            m = __import__(package, globals(), locals(), [engine], -1)
+            return getattr(m, engine)
 
-        eval('return simplejson')
+        return __import__(engine)
+
     except ImportError:
         return False
 
 
-# print _import('simplejson')
-# from .packages import simplejson
+
+for k, v in json_map.items():
+    # print k
+    __engine = _import(k)
+    if __engine:
+        engine, _engine = k, v
+
+        for i in (0, 1):
+            # print __engine
+            _engine[i] = getattr(__engine, _engine[i])
+
+        break
+
+
 
 
 def loads(s):
     """Loads JSON object."""
     try:
-        return internal_json.loads(s)
-    except (ValueError,), why:
+        return _engine[0](s)
+
+    except  _engine[2], why:
         raise JSONError(why)
 
 
@@ -39,8 +70,8 @@ def loads(s):
 def dumps(o):
     """Dumps JSON object."""
     try:
-        return internal_json.dumps(o)
-    except (ValueError,), why:
+        return _engine[1](o)
+    except _engine[2], why:
         raise JSONError(why)
 
 
