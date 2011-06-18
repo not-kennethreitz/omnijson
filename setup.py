ae76452af4df58e8236e8384d7cb95dc832d11e6
Kenneth Reitz <me@kennethreitz.com>
2011-06-18 14:25:18 -0400

    Packaging

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from distutils.core import setup
+
+import omnijson
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+if sys.argv[-1] == 'speedups':
+    try:
+        import pip
+    except ImportError:
+        print('Pip required.')
+        sys.exit(1)
+
+    os.system('pip install ujson')
+    sys.exit()
+
+setup(
+    name='omnijson',
+    version=omnijson.__version__,
+    description='Wraps the best JSON installed, falling back on an internal simplejson.',
+    long_description=open('README.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/omnijson',
+    packages= [
+        'omnijson',
+        'omnijson.packages',
+        'omnijson.packages.simplejson'
+    ],
+    install_requires=required,
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+        # 'Programming Language :: Python :: 3.2',
+    ),
+)
